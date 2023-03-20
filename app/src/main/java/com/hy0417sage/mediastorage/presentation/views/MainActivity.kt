33b209c1c2b5b46d4f142c5f056d6e277cd094e3
Song HyunYoung <97173983+hy0417sage@users.noreply.github.com>
2023-03-20 21:25:46 +0900

    ✨feat: 뷰 바인딩 사용, 검색 기능 구현
    - 검색 기능 구현
    - ktx 사용
    - 뷰 바인딩 사용

--- app/src/main/java/com/hy0417sage/mediastorage/presentation/views/MainActivity.kt
+++ app/src/main/java/com/hy0417sage/mediastorage/presentation/views/MainActivity.kt
@@ -2,13 +2,23 @@ package com.hy0417sage.mediastorage.presentation.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.hy0417sage.mediastorage.R
+import androidx.activity.viewModels
+import com.hy0417sage.mediastorage.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            //TODO 빈 값 검색 시 HTTP 400 오류처리
+            mainViewModel.thumbnailSearch(binding.searchEditText.text.toString())
+        }
     }
 }
--- app/src/main/java/com/hy0417sage/mediastorage/presentation/views/MainViewModel.kt
+++ app/src/main/java/com/hy0417sage/mediastorage/presentation/views/MainViewModel.kt
@@ -1,8 +1,11 @@
 package com.hy0417sage.mediastorage.presentation.views
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hy0417sage.mediastorage.domain.usecase.GetUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +13,12 @@ class MainViewModel @Inject constructor(
     private val getUseCase: GetUseCase,
 ) : ViewModel() {
 
+    fun thumbnailSearch(subject: String) {
+        viewModelScope.launch {
+            val quotes = getUseCase.getFlowData(subject)
+            quotes.collect { data ->
+                Log.d(this.toString(), "검색결과 확인 : $data")
+            }
+        }
+    }
 }
